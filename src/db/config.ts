e107103b7d6729a8e9f9eb221b0d60e0174f32f3
Nora Rodriguez <gia.drago4@gmail.com>
2022-09-09 06:58:31 -0500

    config db changed

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,33 +3,17 @@ import 'dotenv/config';
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
-export const conn =
-    process.env.NODE_ENV === "production"
-        ? new Sequelize({
-            database: DB_NAME,
-            dialect: "postgres",
-            host: DB_HOST,
-
-            username: DB_USER,
-            password: DB_PASSWORD,
-            pool: {
-                max: 3,
-                min: 1,
-                idle: 10000,
-            },
-            dialectOptions: {
-                ssl: {
-                    require: true,
-                    // Ref.: https://github.com/brianc/node-postgres/issues/2009
-                    rejectUnauthorized: false,
-                },
-                keepAlive: true,
-            },
-            ssl: true,
-        })
-        : new Sequelize(
-            `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-
-
-            { logging: false, native: false }
-        );
+export const conn = new Sequelize({
+    database: DB_NAME,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true, // This will help you. But you will see nwe error
+            rejectUnauthorized: false // This line will fix new error
+        }
+    },
+});
